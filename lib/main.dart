0b041cacc40b98c2a69fafc4bc75203793e192be
Bluescope Information Technology Private Limited <ramkumaru2001@gmail.com>
2023-04-03 10:26:17 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:animated_card/views/homepage.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+} 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
